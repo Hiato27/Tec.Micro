13f26b37aab54b1c56d62f994162072260c58ee8
Hiato27 <84791904+Hiato27@users.noreply.github.com>
2025-11-14 14:26:22 -0300

    Update Problema_A:EsclavoSPI.c

--- Laboratorios/Lab4/Codigos/Problema_A:EsclavoSPI.c
+++ Laboratorios/Lab4/Codigos/Problema_A:EsclavoSPI.c
@@ -1,48 +1,100 @@
-
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdint.h>
+#include <avr/interrupt.h>
 #include <stdbool.h>
 
-#define MOSI_PIN  PB3
-#define MISO_PIN  PB4
-#define SCK_PIN   PB5
-#define SS_PIN    PB2
+#define MOTOR_IN1       PD2
+#define MOTOR_IN2       PD3
+#define MOTOR_PWM_PIN   PD6
+#define LED_PWM_PIN     PD5
+#define BUZZER_PIN      PB1
 
-#define LED_SLAVE_PIN  PD6
-
-static void spi_slave_init(void){
-    DDRB &= ~((1<<MOSI_PIN)|(1<<SCK_PIN)|(1<<SS_PIN));
-    DDRB |=  (1<<MISO_PIN);
+static inline void spi_slave_init(void){
+    DDRB &= ~((1<<PB3)|(1<<PB5)|(1<<PB2));
+    DDRB |=  (1<<PB4);
     SPCR = (1<<SPE);
 }
-
-static void led_init(void){
-    DDRD |= (1<<LED_SLAVE_PIN);
-    PORTD &= ~(1<<LED_SLAVE_PIN);
+static inline uint8_t spi_recv(void){
+    while(!(SPSR & (1<<SPIF)));
+    return SPDR;
 }
 
-static inline void led_on(void){  PORTD |=  (1<<LED_SLAVE_PIN); }
-static inline void led_off(void){ PORTD &= ~(1<<LED_SLAVE_PIN); }
+static inline void pwm_init(void){
+    DDRD |= (1<<MOTOR_PWM_PIN) | (1<<LED_PWM_PIN);
+    TCCR0A = (1<<COM0A1) | (1<<COM0B1) | (1<<WGM01) | (1<<WGM00);
+    TCCR0B = (1<<CS01) | (1<<CS00);
+    OCR0A = 200;
+    OCR0B = 0;
+}
+
+static inline void motor_gpio_init(void){
+    DDRD |= (1<<MOTOR_IN1) | (1<<MOTOR_IN2);
+    PORTD &= ~((1<<MOTOR_IN1)|(1<<MOTOR_IN2));
+}
+static inline void motor_set_dir(uint8_t dir){
+    switch(dir){
+        case 1:
+        PORTD |=  (1<<MOTOR_IN1);
+        PORTD &= ~(1<<MOTOR_IN2);
+        break;
+        case 2:
+        PORTD &= ~(1<<MOTOR_IN1);
+        PORTD |=  (1<<MOTOR_IN2);
+        break;
+        default:
+        PORTD &= ~((1<<MOTOR_IN1)|(1<<MOTOR_IN2));
+        break;
+    }
+}
+
+static inline void buzzer_init(void){
+    DDRB |= (1<<BUZZER_PIN);
+    TCCR1A = 0;
+    TCCR1B = 0;
+    OCR1A  = 499;
+    TCCR1A &= ~(1<<COM0A0);
+    PORTB  &= ~(1<<BUZZER_PIN);
+    TCCR1B = (1<<WGM12) | (1<<CS11);
+}
+static inline void buzzer_on(void){
+    TCCR1A |= (1<<COM1A0);
+}
+static inline void buzzer_off(void){
+    TCCR1A &= ~(1<<COM1A0);
+    PORTB  &= ~(1<<BUZZER_PIN);
+}
+
+static inline bool packet_ok(uint8_t s, uint8_t c, uint8_t v, uint8_t k){
+    return (s==0xAA) && ((s ^ c ^ v) == k);
+}
 
 int main(void){
     spi_slave_init();
-    led_init();
+    pwm_init();
+    motor_gpio_init();
+    buzzer_init();
 
-    uint8_t cmd;
-    SPDR = 0x00;
+    uint8_t sync, cmd, val, chk;
 
     while(1){
-        while(!(SPSR & (1<<SPIF)));
-        cmd = SPDR;
+        sync = spi_recv();
+        cmd  = spi_recv();
+        val  = spi_recv();
+        chk  = spi_recv();
 
-        if(cmd == 0xA5){
-            SPDR = 0x5A;
-            led_on();
-        } else {
-            SPDR = 0x00;
-            led_off();
+        if(!packet_ok(sync, cmd, val, chk)){
+            continue;
+        }
+
+        if(cmd == 0x01){
+            motor_set_dir(val);
+        } else if(cmd == 0x02){
+            OCR0B = val;
+        } else if(cmd == 0x04){
+            if(val) buzzer_on();
+            else    buzzer_off();
         }
     }
+    return 0;
 }
