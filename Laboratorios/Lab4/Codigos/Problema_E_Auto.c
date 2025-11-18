661a86a7fcd46c47ca8f7eebcf6568857d63cd96
matiashernandez-star <matias.hernandez@estudiantes.utec.edu.uy>
2025-11-18 20:33:53 -0300

    Update Problema_E_Auto.c

--- Laboratorios/Lab4/Codigos/Problema_E_Auto.c
+++ Laboratorios/Lab4/Codigos/Problema_E_Auto.c
@@ -2,14 +2,31 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdint.h>
+
+#define BAUD 9600UL
+#define UBRR_VAL ((F_CPU / (16UL * BAUD)) - 1)
 
 #define PIN_1 PD7
 #define PIN_2 PD6
 #define PIN_3 PD5
 #define PIN_4 PD4
 
+void uart_init(void) {
+	UBRR0H = (uint8_t)(UBRR_VAL >> 8);
+	UBRR0L = (uint8_t)(UBRR_VAL & 0xFF);
+
+	UCSR0B = (1 << TXEN0) | (1 << RXEN0);
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+char uart_read(void) {
+	while (!(UCSR0A & (1 << RXC0)));
+	return UDR0;
+}
+
 void pwm_init(void) {
-	DDRB |= (1 << PB1) | (1 << PB2); // Configuramos los pines de salida para PWM
+	DDRB |= (1 << PB1) | (1 << PB2);
 	TCCR1A = (1 << WGM10);
 	TCCR1B = (1 << WGM12);
 	TCCR1A |= (1 << COM1A1) | (1 << COM1B1);
@@ -61,11 +78,12 @@ void ejecutar_comando(char c) {
 }
 
 int main(void) {
+	uart_init();
 	pwm_init();
 	direccion_init();
 
 	while (1) {
-		char c = 'F'; // Ejemplo de comando
+		char c = uart_read(); // Espera un carácter de UART
 		ejecutar_comando(c);
 	}
 }
