5ddb37ca38a96fbda62d8340e795a085362fc65a
matiashernandez-star <matias.hernandez@estudiantes.utec.edu.uy>
2025-11-18 20:18:33 -0300

    Update Problema_E_Auto.c

--- Laboratorios/Lab4/Codigos/Problema_E_Auto.c
+++ Laboratorios/Lab4/Codigos/Problema_E_Auto.c
@@ -8,6 +8,24 @@
 #define PIN_3 PD5
 #define PIN_4 PD4
 
+void pwm_init(void) {
+	DDRB |= (1 << PB1) | (1 << PB2); // Configuramos los pines de salida para PWM
+	TCCR1A = (1 << WGM10);
+	TCCR1B = (1 << WGM12);
+	TCCR1A |= (1 << COM1A1) | (1 << COM1B1);
+	TCCR1B |= (1 << CS11) | (1 << CS10); // Prescaler 64
+	OCR1A = 0;
+	OCR1B = 0;
+}
+
+void motor_izq(uint8_t duty) {
+	OCR1A = duty;
+}
+
+void motor_der(uint8_t duty) {
+	OCR1B = duty;
+}
+
 void direccion_adelante(void) {
 	PORTD |=  (1 << PIN_1) | (1 << PIN_3);
 	PORTD &= ~((1 << PIN_2) | (1 << PIN_4));
@@ -27,10 +45,14 @@ void ejecutar_comando(char c) {
 	switch (c) {
 		case 'F':
 		direccion_adelante();
+		motor_izq(255);
+		motor_der(255);
 		break;
 
 		case 'G':
 		direccion_atras();
+		motor_izq(255);
+		motor_der(255);
 		break;
 
 		default:
@@ -39,6 +61,7 @@ void ejecutar_comando(char c) {
 }
 
 int main(void) {
+	pwm_init();
 	direccion_init();
 
 	while (1) {
